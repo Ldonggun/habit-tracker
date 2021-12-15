fb08152458529c06f0101b92fecad9c5e5e6fb8f
Ldonggun <89460880+Ldonggun@users.noreply.github.com>
2021-12-15 22:41:39 +0900

    이벤트 처리하기

--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -9,11 +9,39 @@ class Habits extends Component {
       { id: 3, name: 'Coding', count: 0 },
     ],
   };
+
+  handleIncrement = habit => {
+    const habits = [...this.state.habits]; // 직접적으로 state를 건들면 좋지 않아서 spreadopertor를 사용 새로운 배열 껍데를 만든것
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({ habits });
+  };
+
+  handleDecrement = habit => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const count = habits[index].count - 1;
+    habits[index].count = count < 0 ? 0 : count;
+    this.setState({ habits });
+  };
+
+  handleDelete = habit => {
+    console.log(`handledelete ${habit.name}`);
+    const habits = this.state.habits.filter(item => item.id !== habit.id);
+    this.setState({ habits });
+  };
+
   render() {
     return (
       <ul>
         {this.state.habits.map(habit => (
-          <Habit key={habit.id} habit={habit} />
+          <Habit
+            key={habit.id}
+            habit={habit}
+            onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
+            onDelete={this.handleDelete}
+          />
         ))}
       </ul>
     );
