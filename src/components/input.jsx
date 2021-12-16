7bd2d820c6974eb4b750aa44f90d8fa9c56a6e19
Ldonggun <89460880+Ldonggun@users.noreply.github.com>
2021-12-16 14:59:48 +0900

    라이프 사이클

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,14 +1,32 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 
-class Input extends Component {
+class Input extends PureComponent {
+  inputRef = React.createRef();
+  formRef = React.createRef();
+
+  onSubmit = e => {
+    e.preventDefault();
+    const name = this.inputRef.current.value;
+    name && this.props.onAdd(name);
+    // this.inputRef.current.value = '';
+    this.formRef.current.reset();
+  };
   render() {
+    console.log('inputForm');
     return (
-      <>
-        <div className='input-container'>
-          <input value='Habit' />
-          <button>Add</button>
-        </div>
-      </>
+      <form
+        ref={this.formRef}
+        className='input-container'
+        onSubmit={this.onSubmit}
+      >
+        <input
+          ref={this.inputRef}
+          type='text'
+          className='add-input'
+          placeholder='Habit'
+        />
+        <button className='add-button'>Add</button>
+      </form>
     );
   }
 }
